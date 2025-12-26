25ed5744b2511e6df8bb6f6487efa315a4def221
celuu <christine.e.luu@gmail.com>
2025-12-26 10:15:40 -0800

    Try clamp.

---
+++ clamp.mjs
@@ -0,0 +1,21 @@
+export default function clamp(value, lower, upper) {
+  if (value >= lower && value <= upper) {
+    return value;
+  }
+
+  if (value < lower) {
+    return lower;
+  }
+  if (value > upper) {
+    return upper;
+  }
+}
+
+// Within the bounds, return as-is.
+console.log(clamp(3, 0, 5)); // => 3
+
+// Smaller than the lower bound.
+console.log(clamp(-10, -3, 5)); // => -3
+
+// Bigger than the upper bound.
+console.log(clamp(10, -5, 5)); // => 5
