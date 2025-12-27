2c1f83e0de09adf65687333e1a37208b8499f76e
celuu <christine.e.luu@gmail.com>
2025-12-27 12:47:28 -0800

    Sum recursion

---
+++ sumNumbersRecursion.js
@@ -0,0 +1,11 @@
+const sumNumbersRecursive = (numbers) => {
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  return numbers[0] + sumNumbersRecursive(numbers.slice(1));
+};
+
+module.exports = {
+  sumNumbersRecursive,
+};
