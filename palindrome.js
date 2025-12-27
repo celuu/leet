f160018f3c8fead288288da7f65c2c1b77001030
celuu <christine.e.luu@gmail.com>
2025-12-27 13:00:31 -0800

    Add palindrome.

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+
+const palindrome = (s) => {
+  if (s.length <= 1) return true;
+  if (s[0] !== s[s.length - 1]) return false;
+  return palindrome(s.slice(1, -1));
+};
+
+module.exports = {
+  palindrome,
+};
