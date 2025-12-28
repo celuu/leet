be54c2b47300e6be51ba59f93329df81d2590f92
celuu <christine.e.luu@gmail.com>
2025-12-28 10:15:02 -0800

    linkedlists.

---
+++ linkedListValues.js
@@ -0,0 +1,20 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListValues = (head) => {
+  let result = [];
+  let current = head;
+  while (current !== null) {
+    result.push(current.val);
+    current = current.next;
+  }
+  return result;
+};
+
+module.exports = {
+  linkedListValues,
+};
