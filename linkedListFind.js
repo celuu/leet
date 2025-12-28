c7c589e78185f8162c2dc2cabbc453755c3bada3
celuu <christine.e.luu@gmail.com>
2025-12-28 10:27:32 -0800

    linkedlistfind.

--- linkedListFind.js
+++ linkedListFind.js
@@ -13,6 +13,15 @@
 //   return linkedListFind(head.next, target);
 // };
 
+const linkedListFind = (head, target) => {
+  let current = head;
+  while (current !== null) {
+    if (current.val === target) return true;
+    current = current.next;
+  }
+  return false;
+};
+
 module.exports = {
   linkedListFind,
 };
