5164cff3e06e34287e430404dd161dd17b9e010f
celuu <christine.e.luu@gmail.com>
2025-12-28 10:38:03 -0800

    get node value.

---
+++ getNodeValue.js
@@ -0,0 +1,30 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const getNodeValue = (head, index) => {
+  let current = head;
+  let count = 0;
+  while (current !== null) {
+    if (count === index) {
+      return current.val;
+    }
+    current = current.next;
+    count++;
+  }
+  return null;
+};
+
+//recursive version
+// const getNodeValue = (head, index) => {
+//   if (head === null) return null;
+//   if (index === 0) return head.val;
+//   return getNodeValue(head.next, index - 1);
+// };
+
+module.exports = {
+  getNodeValue,
+};
