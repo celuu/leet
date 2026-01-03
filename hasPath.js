2c7ec6d4bea65f430ce1b9d56cdf2a513bc081ee
celuu <christine.e.luu@gmail.com>
2026-01-03 15:10:31 -0800

    has path.

---
+++ hasPath.js
@@ -0,0 +1,16 @@
+const hasPath = (graph, src, dst) => {
+  let stack = [src];
+
+  while (stack.length > 0) {
+    let current = stack.pop();
+    if (current === dst) return true;
+    for (let neighbor of graph[current]) {
+      stack.push(neighbor);
+    }
+  }
+  return false;
+};
+
+module.exports = {
+  hasPath,
+};
